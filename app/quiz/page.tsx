109dd20901275edccfbe13aa7375217d6ef80d03
Fabian Haas <29468630+hfxbse@users.noreply.github.com>
2024-02-06 18:19:42 +0100

    add button to continue to next question

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -5,8 +5,13 @@ import styles from './page.module.css'
 
 export default function Quiz() {
     return <div className={styles.page}>
-        <AnswerPicker id={"02faf8925f845a6438f9997dfbe579e3"} category={TestCategory.FLUTTER_COPILOT}/>
-        <AnswerPicker id={"02faf8925f845a6438f9997dfbe579e3"} category={TestCategory.FLUTTER_WELLTESTED}/>
-        <AnswerPicker id={"02faf8925f845a6438f9997dfbe579e3"} category={TestCategory.FLUTTER_HUMAN}/>
+        <div className={styles.snippets}>
+            <AnswerPicker id={"02faf8925f845a6438f9997dfbe579e3"} category={TestCategory.FLUTTER_COPILOT}/>
+            <AnswerPicker id={"02faf8925f845a6438f9997dfbe579e3"} category={TestCategory.FLUTTER_WELLTESTED}/>
+            <AnswerPicker id={"02faf8925f845a6438f9997dfbe579e3"} category={TestCategory.FLUTTER_HUMAN}/>
+        </div>
+        <div>
+            <button>Continue to next quiz</button>
+        </div>
     </div>
 }
