6d54e1e5ed68ddbb6485e746f2493bb214c043f9
Fabian Haas <29468630+hfxbse@users.noreply.github.com>
2024-02-06 17:46:18 +0100

    create quiz page answer picker layout

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -1,9 +1,12 @@
 'use client'
 
 import AnswerPicker, {TestCategory} from "@/app/quiz/answer-picker";
+import styles from './page.module.css'
 
 export default function Quiz() {
-    return <>
+    return <div className={styles.page}>
         <AnswerPicker id={"02faf8925f845a6438f9997dfbe579e3"} category={TestCategory.FLUTTER_COPILOT}/>
-    </>
+        <AnswerPicker id={"02faf8925f845a6438f9997dfbe579e3"} category={TestCategory.FLUTTER_WELLTESTED}/>
+        <AnswerPicker id={"02faf8925f845a6438f9997dfbe579e3"} category={TestCategory.FLUTTER_HUMAN}/>
+    </div>
 }
