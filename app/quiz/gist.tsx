a8a43a4dd03e6a6dcbf4fe85ea5484f73a8813b5
Fabian Haas <29468630+hfxbse@users.noreply.github.com>
2024-02-12 13:08:58 +0100

    add code snippet loading indicator

--- app/quiz/gist.tsx
+++ app/quiz/gist.tsx
@@ -1,5 +1,34 @@
 import SuperGist from 'super-react-gist'
+import {BarLoader} from "react-spinners";
+import {Roboto} from "next/font/google";
+import styles from './gist.module.css'
 
-export default function Gist({id, username, file}: {id: string, username: string, file: string}) {
-    return <SuperGist url={`https://gist.github.com/${username}/${id}`} file={file}/>
+const roboto = Roboto({weight: "300", subsets: ["latin"]})
+
+function placeholder() {
+    return <div className={styles.placeholder}>
+        <div>
+            <h2 className={roboto.className}>Loading code snippet…</h2>
+            <BarLoader
+                color={"var(--color-primary)"}
+                cssOverride={{
+                    background: "inherit"
+                }}
+            />
+        </div>
+    </div>
+}
+
+export default function Gist({id, username, file, className}: {
+    id: string,
+    username: string,
+    file: string,
+    className?: string
+}) {
+    return <SuperGist
+        url={`https://gist.github.com/${username}/${id}`}
+        file={file}
+        LoadingComponent={placeholder}
+        className={className}
+    />
 }
